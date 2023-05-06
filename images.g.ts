e86c9f07dbaa0ec7da295edf9728b5090c26d7f2
m3211n <s.hanushchak@outlook.com>
2023-05-06 22:51:40 +0200

    new project

---
+++ images.g.ts
@@ -0,0 +1,323 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "J":return img`
+8 8 8 8 8 . . . . . . . . . . 
+8 8 8 8 8 . . . . . . . . . . 
+8 8 8 8 8 . . . . . . . . . . 
+8 8 8 8 8 . . . . . . . . . . 
+8 8 8 8 8 . . . . . . . . . . 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "L":return img`
+. . . . . . . . . . 4 4 4 4 4 
+. . . . . . . . . . 4 4 4 4 4 
+. . . . . . . . . . 4 4 4 4 4 
+. . . . . . . . . . 4 4 4 4 4 
+. . . . . . . . . . 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "I":return img`
+....................
+....................
+....................
+....................
+....................
+99999999999999999999
+99999999999999999999
+99999999999999999999
+99999999999999999999
+99999999999999999999
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+`;
+            case "image4":
+            case "O":return img`
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+5 5 5 5 5 5 5 5 5 5 
+`;
+            case "image6":
+            case "T":return img`
+. . . . . a a a a a . . . . . 
+. . . . . a a a a a . . . . . 
+. . . . . a a a a a . . . . . 
+. . . . . a a a a a . . . . . 
+. . . . . a a a a a . . . . . 
+a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "S":return img`
+. . . . . 7 7 7 7 7 7 7 7 7 7 
+. . . . . 7 7 7 7 7 7 7 7 7 7 
+. . . . . 7 7 7 7 7 7 7 7 7 7 
+. . . . . 7 7 7 7 7 7 7 7 7 7 
+. . . . . 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 . . . . . 
+7 7 7 7 7 7 7 7 7 7 . . . . . 
+7 7 7 7 7 7 7 7 7 7 . . . . . 
+7 7 7 7 7 7 7 7 7 7 . . . . . 
+7 7 7 7 7 7 7 7 7 7 . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image8":
+            case "txt_lines":return img`
+1 . . . 1 1 1 . 1 . . . 1 . 1 1 1 . 1 1 1 1 
+1 . . . . 1 . . 1 1 . . 1 . 1 . . . 1 . . . 
+1 . . . . 1 . . 1 . 1 . 1 . 1 1 1 . 1 1 1 1 
+1 . . . . 1 . . 1 . . 1 1 . 1 . . . . . . 1 
+1 1 1 . 1 1 1 . 1 . . . 1 . 1 1 1 . 1 1 1 1 
+`;
+            case "image9":
+            case "txt_score":return img`
+1 1 1 1 . 1 1 1 . 1 1 1 1 . 1 1 1 1 1 . 1 1 1 
+1 . . . . 1 . . . 1 . . 1 . . 1 . . 1 . 1 . . 
+1 1 1 1 . 1 . . . 1 . . 1 . . 1 1 1 1 . 1 1 1 
+. . . 1 . 1 . . . 1 . . 1 . . 1 . 1 . . 1 . . 
+1 1 1 1 . 1 1 1 . 1 1 1 1 . . 1 . . 1 . 1 1 1 
+`;
+            case "image10":
+            case "txt_hiscore":return img`
+1 . . 1 . 1 . . . . 1 1 1 1 . 1 1 1 . 1 1 1 1 . 1 1 1 1 1 . 1 1 1 
+1 . . 1 . 1 . . . . 1 . . . . 1 . . . 1 . . 1 . . 1 . . 1 . 1 . . 
+1 1 1 1 . 1 . 1 1 . 1 1 1 1 . 1 . . . 1 . . 1 . . 1 1 1 1 . 1 1 1 
+1 . . 1 . 1 . . . . . . . 1 . 1 . . . 1 . . 1 . . 1 . 1 . . 1 . . 
+1 . . 1 . 1 . . . . 1 1 1 1 . 1 1 1 . 1 1 1 1 . . 1 . . 1 . 1 1 1 
+`;
+            case "image11":
+            case "txt_next":return img`
+1 . . . 1 . 1 1 1 . 1 . . . 1 . 1 1 1 1 1 
+1 1 . . 1 . 1 . . . . 1 . 1 . . . . 1 . . 
+1 . 1 . 1 . 1 1 1 . . . 1 . . . . . 1 . . 
+1 . . 1 1 . 1 . . . . 1 . 1 . . . . 1 . . 
+1 . . . 1 . 1 1 1 . 1 . . . 1 . . . 1 . . 
+`;
+            case "image12":
+            case "txt_level":return img`
+1 . . . 1 1 1 . 1 . . . 1 . 1 1 1 . 1 . . 
+1 . . . 1 . . . 1 . . . 1 . 1 . . . 1 . . 
+1 . . . 1 1 1 . 1 . . . 1 . 1 1 1 . 1 . . 
+1 . . . 1 . . . . 1 . 1 . . 1 . . . 1 . . 
+1 1 1 . 1 1 1 . . . 1 . . . 1 1 1 . 1 1 1 
+`;
+            case "image13":
+            case "J0":return img`
+c c c c c . . . . . . . . . . 
+c c c c c . . . . . . . . . . 
+c c c c c . . . . . . . . . . 
+c c c c c . . . . . . . . . . 
+c c c c c . . . . . . . . . . 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image14":
+            case "L0":return img`
+. . . . . . . . . . c c c c c 
+. . . . . . . . . . c c c c c 
+. . . . . . . . . . c c c c c 
+. . . . . . . . . . c c c c c 
+. . . . . . . . . . c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image15":
+            case "I0":return img`
+....................
+....................
+....................
+....................
+....................
+cccccccccccccccccccc
+cccccccccccccccccccc
+cccccccccccccccccccc
+cccccccccccccccccccc
+cccccccccccccccccccc
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+....................
+`;
+            case "image16":
+            case "O0":return img`
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+c c c c c c c c c c 
+`;
+            case "image17":
+            case "T0":return img`
+. . . . . c c c c c . . . . . 
+. . . . . c c c c c . . . . . 
+. . . . . c c c c c . . . . . 
+. . . . . c c c c c . . . . . 
+. . . . . c c c c c . . . . . 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image18":
+            case "S0":return img`
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image19":
+            case "Z0":return img`
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+c c c c c c c c c c . . . . . 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . c c c c c c c c c c 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "Z":return img`
+2 2 2 2 2 2 2 2 2 2 . . . . . 
+2 2 2 2 2 2 2 2 2 2 . . . . . 
+2 2 2 2 2 2 2 2 2 2 . . . . . 
+2 2 2 2 2 2 2 2 2 2 . . . . . 
+2 2 2 2 2 2 2 2 2 2 . . . . . 
+. . . . . 2 2 2 2 2 2 2 2 2 2 
+. . . . . 2 2 2 2 2 2 2 2 2 2 
+. . . . . 2 2 2 2 2 2 2 2 2 2 
+. . . . . 2 2 2 2 2 2 2 2 2 2 
+. . . . . 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
+
+// Auto-generated code. Do not edit.
+namespace myTiles {
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,215 @@
+class Bag {
+    private preview: Sprite
+    private contents: number[]
+
+    constructor() {
+        let full = false
+        this.contents = []
+        this.fill()
+        this.preview = sprites.create(image.create(4 * CELL_SIZE, 15 * CELL_SIZE))
+        this.preview.setPosition(134, 73)
+    }
+
+    private fill() {
+        let full = false
+        while (!full) {
+            let rnd = Math.randomRange(0, 6)
+            if (this.contents.indexOf(rnd) == -1) {
+                this.contents.push(rnd)
+            }
+            if (this.contents.length == 7) {
+                full = true
+            }
+        }
+    }
+
+    private updateQueue() {
+        this.preview.image.fill(0)
+        for (let i = 0; i < 3; i++) {
+            this.preview.image.drawImage(shapes[this.contents[i]], 0, i * 20 + 5)
+        }
+    }
+
+    deal(): number {
+        let next = this.contents.shift()
+        if (this.contents.length < NEXT_PIECES) {
+            this.fill()
+        }
+        this.updateQueue()
+        return next
+    }
+}
+
+class Tetrimino {
+    colors: number[][]
+    piece: Sprite
+    rotation: number
+    shapeID: number
+    x: number
+    y: number
+    w: number
+    h: number
+    bottom: number
+
+    private ghost_piece: Sprite
+
+    constructor() {
+        this.respawn()
+    }
+
+    private getColorsArray(): number[][] {
+        let result: number[][] = []
+        let img = this.piece.image
+        let s = img.width / CELL_SIZE 
+        for (let y = 0; y < s; y++) {
+            result.push([])
+            for (let x = 0; x < s; x++) {
+                result[y].push(img.getPixel(x * CELL_SIZE, y * CELL_SIZE))
+            }
+        }
+        return result
+    }
+
+    private spawn_ghost() {
+        let img = shapes[this.shapeID + 7]
+        if (this.rotation != 0) {
+            img = img.rotated(this.rotation * 90)
+        }
+        this.ghost_piece = sprites.create(img)
+        this.ghost_piece.setPosition(X0 + (this.x * CELL_SIZE + this.ghost_piece.width / 2), Y0 + (this.bottom * CELL_SIZE + this.ghost_piece.height / 2))
+    }
+
+    respawn() {
+        this.x = (this.shapeID == 3) ? 4 : 3
+        this.y = 0
+        this.shapeID = bag.deal()
+        this.w = (this.shapeID == 0) ? 4 : (this.shapeID == 3 ? 2 : 3)
+        this.h = heights[this.shapeID][this.y]
+        this.bottom = MATRIX_HEIGHT - this.h
+        this.respawnAt(this.x, this.y, Rotation.Z)
+    }
+
+    respawnAt(x: number, y: number, r: number) {
+        this.x = x
+        this.y = y
+        this.rotation = r
+        this.piece = sprites.create(shapes[this.shapeID])
+        this.colors = this.getColorsArray()
+        this.piece.setPosition(X0 + (this.x * CELL_SIZE + this.piece.width / 2), Y0 + (this.y * CELL_SIZE + this.piece.height / 2))
+        this.spawn_ghost()
+    }
+
+    rotate(cw: boolean) {
+        let canRotate = true
+        let next_rotation = cw ? ((this.rotation == 3) ? 0 : this.rotation + 1) : ((this.rotation == 0) ? 3 : this.rotation - 1)
+        if (canRotate) {
+            this.rotation = next_rotation
+            this.piece.setImage(this.piece.image.rotated(cw ? 90 : -90))
+            this.colors = this.getColorsArray() // update colors array after the rotation
+        }
+    }
+}
+
+// FUNCTIONS ----------------------------------------------
+
+function updateStats() {
+
+    sScore.setText(score.toString())
+    sLevel.setText(level.toString())
+    sLines.setText(lines.toString())
+    sHighscore.setText(highscore.toString())
+    sLevel.setPosition(6 + sLevel.width / 2, 47 + sLevel.height / 2)
+    sLines.setPosition(6 + sLines.width / 2, 72 + sLines.height / 2)
+    sScore.setPosition(6 + sScore.width / 2, 22 + sScore.height / 2)
+    sHighscore.setPosition(6 + sHighscore.width / 2, 97 + sHighscore.height / 2)
+}
+
+// SETTINGS -----------------------------------------------
+
+const CELL_SIZE = 5
+const NEXT_PIECES = 3
+const MATRIX_HEIGHT = 22
+const X0 = 55
+const Y0 = 5
+
+// DATA ---------------------------------------------------
+
+const shapes: Image[] = [
+    assets.image`I`,
+    assets.image`J`,
+    assets.image`L`,
+    assets.image`O`,
+    assets.image`S`,
+    assets.image`T`,
+    assets.image`Z`,
+    assets.image`I0`,
+    assets.image`J0`,
+    assets.image`L0`,
+    assets.image`O0`,
+    assets.image`S0`,
+    assets.image`T0`,
+    assets.image`Z0`
+
+]
+
+const heights = [
+    [2, 4, 3, 4],
+    [2, 3, 3, 3],
+    [2, 3, 3, 3],
+    [2, 2, 2, 2],
+    [2, 3, 3, 3],
+    [2, 3, 3, 3],
+    [2, 3, 3, 3]
+]
+
+enum Rotation {
+    Z = 0,
+    R = 1,
+    T = 2,
+    L = 3
+}
+
+// UI -----------------------------------------------------
+
+let bg = image.create(160, 120)
+bg.fillRect(X0 - 3, Y0 - 3, 56, 116, 11)          // Matrix
+bg.fillRect(X0 - 1, Y0 - 1, 52, 112, 0)
+scene.setBackgroundImage(bg)
+
+// STATS --------------------------------------------------
+
+let sScoreTitle = sprites.create(assets.image`txt_score`)
+let sLevelTitle = sprites.create(assets.image`txt_level`)
+let sLinesTitle = sprites.create(assets.image`txt_lines`)
+let sHighscoreTitle = sprites.create(assets.image`txt_hiscore`)
+let sNextTitle = sprites.create(assets.image`txt_next`)
+
+sScoreTitle.setPosition(18, 18)
+sLevelTitle.setPosition(18, 43)
+sLinesTitle.setPosition(18, 68)
+sHighscoreTitle.setPosition(23, 93)
+sNextTitle.setPosition(134, 18)
+
+let sScore = textsprite.create("0", 0, 11)
+sScore.setMaxFontHeight(5)
+let sLevel = textsprite.create("0", 0, 11)
+sLevel.setMaxFontHeight(5)
+let sLines = textsprite.create("0", 0, 11)
+sLines.setMaxFontHeight(5)
+let sHighscore = textsprite.create("0", 0, 11)
+sHighscore.setMaxFontHeight(5)
+
+// GAME ---------------------------------------------------
+
+let level: number = 1
+let score: number = 0
+let gravity: number = 1
+let lines: number = 0
+let highscore: number = 0
+
+let bag = new Bag()
+let t = new Tetrimino()
+
+let im = image.create(10, 10)
+
+updateStats()
